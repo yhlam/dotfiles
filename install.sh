48652691f5be446936d87f5b0938298fe5b34f95
Hei <lamyuenhei@gmail.com>
2021-04-09 00:05:27 +0800

    Update zsh config

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -s $DIR/tmux.conf $HOME/.tmux.conf
 ln -s $DIR/gitconfig $HOME/.gitconfig
 ln -s $DIR/inputrc $HOME/.inputrc
 ln -s $DIR/bashrc_extras $HOME/.bashrc_extras
+ln -s $DIR/zshrc $HOME/.zshrc
 mkdir -p $HOME/.vim/pack
 ln -s -T $DIR/vim-plugins $HOME/.vim/pack/plugins
 
